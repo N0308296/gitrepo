c56e5fa96e032e306e86db481975f3be98c1a8d6
Mestre <n0308296@lm.lmig.com>
2021-05-31 16:25:35 +0100

    teste

---
+++ teste.java
@@ -0,0 +1,20 @@
+package Principal;
+
+import java.time.LocalDate;
+
+public class aula1_main {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        // Data de Nascimento: 22-03-1986
+
+        System.out.println(" ");
+
+        LocalDate localDate = LocalDate.now();
+        System.out.print("Data corrente: ");
+        System.out.print(localDate.getDayOfMonth());
+        System.out.print("-");
+        System.out.print(localDate.getMonthValue());
+        System.out.print("-");
+        System.out.print(localDate.getYear());
+    }
+}
